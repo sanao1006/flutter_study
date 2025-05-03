c1f23d2e48c59e66c6c8e5be0f5c84f466a63cbb
sanao1006 <naosak1006@gmail.com>
2025-05-03 15:58:18 +0900

    feat: implement FavoritesPage to display user's favorite items

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
         break;
 
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
 
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -175,5 +175,30 @@ class BigCard extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return Column(children: [
+      Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text('You have '
+            '${appState.favorites.length} favorites:'),
+      ),
+      for (var msg in appState.favorites)
+        ListTile(
+          leading: Icon(Icons.favorite),
+          title: Text(msg.asLowerCase),
+        )
+    ],);
   }
 }
