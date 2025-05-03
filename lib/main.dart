12d6098a1c92f0a0009ea4eeb2be1bb27bca0467
sanao1006 <naosak1006@gmail.com>
2025-05-03 15:41:11 +0900

    feat: add NavigationRail for improved navigation between Home and Favorites

--- lib/main.dart
+++ lib/main.dart
@@ -49,8 +49,35 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: GeneratorPage(),
+      body: Row(
+        children: [
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text("Home"),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.favorite),
+                  label: Text("Favorites"),
+                ),
+              ],
+              selectedIndex: 0,
+              onDestinationSelected: (value) {
+                print("Selected index: $value");
+              },
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: GeneratorPage(),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
